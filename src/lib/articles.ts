148a0dab66e16bd73e27a5575f5e9dbdf9228143
Amanda Martins Xavier <hi@amandamartins.dev>
2023-09-27 19:48:19 -0300

    :fire: Deleting the old Astro website

--- src/lib/articles.ts
+++
@@ -1,7 +0,0 @@
-export const getArticles = async () => {
-  const response = await fetch(
-    "https://dev.to/api/articles?username=pixelmandy",
-  );
-  const articles = await response.json();
-  return articles;
-};
