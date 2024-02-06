8dd597cb5e3779a282bc9f339e607c845e7e8481
Amanda Martins Xavier <hi@amandamartins.dev>
2024-02-06 18:03:24 -0300

    :adhesive_bandage: Fix: favicon, apple-icon and icon default configuration

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,18 +8,6 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#e09f3e",
-    theme_color: "#e09f3e",
-    icons: [
-      {
-        src: "/apple-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-      {
-        src: "/icon.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-    ],
+    theme_color: "#e09f3e"
   };
 }
