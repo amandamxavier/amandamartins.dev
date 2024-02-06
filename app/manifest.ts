0cbb30877d4055564efd69a9f8be24ac039acb61
Amanda Martins Xavier <hi@amandamartins.dev>
2024-02-06 17:52:17 -0300

    :mag: Update: SEO infos

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,12 +7,17 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "Fullstack Developer, based in São Paulo, Brazil.",
     start_url: "/",
     display: "standalone",
-    background_color: "#000000",
-    theme_color: "#000000",
+    background_color: "#e09f3e",
+    theme_color: "#e09f3e",
     icons: [
       {
         src: "/apple-icon.png",
         sizes: "180x180",
+        type: "image/png",
+      },
+      {
+        src: "/icon.png",
+        sizes: "32x32",
         type: "image/png",
       },
     ],
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 1,
     },
+    {
+      url: "https://amandamartins.dev/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://amandamartins.dev/resume",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
